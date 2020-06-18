24728488dbd41846b5708a37b7c27e5c88814d49
Nhien Dang <danghaonhien@gmail.com>
2020-06-17 21:53:43 -0700

    Heroku

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 // Setup middlewares
 // app.get("/", (req, res) => res.send("API running"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.get("/", (req, res) => res.send("API running"));
+// app.get("/", (req, res) => res.send("API running"));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/client/build/index.html"));
+  });
 }
 app.use(routes);
 require("./services/passport");
